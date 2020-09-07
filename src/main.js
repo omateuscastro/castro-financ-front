17cdf7fd2e2c750942cf805b3183378b90a71fa0
Mateus Castro <mcastro.mateus@gmail.com>
2020-09-07 19:13:05 -0300

    Ajuste no locale do Moment.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from '@/router'
 import store from '@/store'
 import vuetify from '@/plugins/vuetify'
 import vuelidate from '@/plugins/vuelidate'
+import moment from '@/plugins/moment'
 import { errorHandler } from '@/utils'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
@@ -16,5 +17,6 @@ new Vue({
   store,
   vuetify,
   vuelidate,
+  moment,
   render: h => h(App)
 }).$mount('#app')
